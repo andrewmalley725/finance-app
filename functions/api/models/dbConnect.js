4764d4f95def239b0f24767eb8142fd5d5eef6b4
andrewmalley725 <andrew.alley99@gmail.com>
2023-12-24 18:42:37 -0600

    firebase deployment

--- functions/api/models/dbConnect.js
+++ functions/api/models/dbConnect.js
@@ -1,14 +1,29 @@
 require('dotenv').config();
 
-const knex = require('knex')({
+const connections = {
+  prod: {
+    client: 'mysql',
+    connection: {
+      host : process.env.DB_HOST,
+      port : 3306,
+      user : process.env.DB_USER,
+      password : process.env.DB_PASS,
+      database : 'finance-app',
+      ssl: {"rejectUnauthorized":false}
+    }
+  },
+  dev: {
     client: 'mysql',
     connection: {
       host : 'localhost',
       port : 3306,
       user : 'root',
-      password : process.env.DB_PASS,
+      password : process.env.DEV_PASS,
       database : 'finance_app'
     }
-});
+  },
+}
+
+const knex = require('knex')();
 
 module.exports = knex;
