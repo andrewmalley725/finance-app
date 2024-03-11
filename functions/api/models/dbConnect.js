f9fa8d4f335c1da6e792d36e934b678919ab9b9e
andrewmalley725 <andrew.alley99@gmail.com>
2024-03-11 02:07:20 -0600

    migrated db

--- functions/api/models/dbConnect.js
+++ functions/api/models/dbConnect.js
@@ -1,7 +1,33 @@
 require('dotenv').config();
-const knex = require('knex');
-const config = require('../../knexfile');
+// const knex = require('knex');
+// const config = require('../../knexfile');
 
-const db = knex(config.development);
+// const db = knex(config.development);
 
-module.exports = db;
+// module.exports = db;
+
+const connections = {
+  prod:  {
+    client: 'mysql',
+    connection: {
+      host : process.env.DB_HOST,
+      port : 3306,
+      user : process.env.DB_USER,
+      password : process.env.DB_PASS,
+      database : 'finance-app',
+      ssl: {"rejectUnauthorized":false}
+    }
+  }
+}
+
+
+function getConnection(){
+  //const env = process.env.NODE_ENV;
+  return connections.prod;
+}
+ 
+
+const knex = require('knex')(getConnection());
+
+
+module.exports = knex;
